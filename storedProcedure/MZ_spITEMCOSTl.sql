62473ad322da0d9b1535744d673a9aff6a5451e2
maranz <luca.marani.78@gmail.com>
2014-08-31 22:02:07 +0200

    add ProjectID and UserId into ItemCost

--- storedProcedure/MZ_spITEMCOSTl.sql
+++ storedProcedure/MZ_spITEMCOSTl.sql
@@ -5,7 +5,9 @@ CREATE PROCEDURE MZ_spITEMCOSTIl (
 		in NAME varchar(255)
 	   ,in APPID varchar(36)
 	   ,in VDATE datetime
-	   ,in CTYPE varchar(1)							 
+	   ,in CTYPE varchar(1)	
+	   ,in USERID varchar(36)
+	   ,in PROJECTID varchar(36)
 )
 BEGIN
 	/*
@@ -13,23 +15,58 @@ BEGIN
 	*/		
 	SET @NAME = TRIM(IFNULL(NAME, ""));
 	SET @APPID = TRIM(IFNULL(APPID, ""));
-	SET @CTYPE = TRIM(IFNULL(CTYPE, ""));
+	SET @CTYPE = TRIM(IFNULL(CTYPE, ""));	
+	SET @USERID = TRIM(IFNULL(USERID, ""));
+	SET @PROJECTID = TRIM(IFNULL(PROJECTID, ""));
 	
-	SET @QRY = concat("select ITEMCOSTID, NAME from MZ_ITEMCOST where cdate is null and APPID = ? and CTYPE = ? ");
+	SET @QRY = concat("select c.ITEMCOSTID, c.NAME
+from MZ_ITEMCOST c
+inner join MZ_ITEMCOSTUSER uc
+on uc.cdate is null 
+{0}
+and uc.ITEMCOSTID = c.ITEMCOSTID
+where c.cdate is null 
+and c.APPID = ? 
+and c.CTYPE = ? 
+{1}
+order by c.NAME");
+	
 	if @NAME != "" then	
-		SET @QRY = concat(@QRY, " and NAME = ?");
+		SET @QRY = replace(@QRY, "{1}", " and c.NAME = ? ");
+	else
+		SET @QRY = replace(@QRY, "{1}", " ");
 	end if;
-	SET @QRY = concat(@QRY, " order by NAME");
+
+	if @PROJECTID != "" then
+		SET @QRY = replace(@QRY, "{0}"," and uc.PROJECTID = ? ");
+	elseif @USERID != "" then
+		SET @QRY = replace(@QRY, "{0}"," and uc.USERID = ? ");
+	else
+		SET @QRY = replace(@QRY, "{0}", " ");
+	end if;
+	
+	#select @QRY;
+
 	PREPARE stmt FROM @QRY; 
 
-	if @NAME != "" then	
-		EXECUTE stmt USING @APPID, @CTYPE, @NAME;
-	else
-		EXECUTE stmt USING @APPID, @CTYPE;
-	end if;
-	
+	case 
+	  	when @NAME != "" and @PROJECTID != "" then 
+	  		EXECUTE stmt USING @PROJECTID, @APPID, @CTYPE, @NAME;	
+		when @NAME = "" and @PROJECTID != "" then 
+			EXECUTE stmt USING @PROJECTID, @APPID, @CTYPE;
+	  	when @NAME != "" and @USERID != "" then 
+	  		EXECUTE stmt USING @USERID, @APPID, @CTYPE, @NAME;
+		when @NAME = "" and @USERID != "" then 
+			EXECUTE stmt USING @USERID, @APPID, @CTYPE;
+	  	when @NAME != "" then 
+	  		EXECUTE stmt USING @APPID, @CTYPE, @NAME;
+	  	else 
+	  		EXECUTE stmt USING @APPID, @CTYPE;	  	
+	end case;
+
 	DEALLOCATE PREPARE stmt;
 
 END; //
 DELIMITER //
+
 
