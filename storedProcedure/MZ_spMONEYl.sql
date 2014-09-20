8fa2dab617f47f186f9ae1b5f7074ec09f542f6b
maranz <luca.marani.78@gmail.com>
2014-09-20 21:32:10 +0200

    create spMoneyl

---
+++ storedProcedure/MZ_spMONEYl.sql
@@ -0,0 +1,73 @@
+DROP PROCEDURE IF EXISTS MZ_spMONEYl;
+
+DELIMITER //
+CREATE PROCEDURE MZ_spMONEYl (		
+		 in YEAR int
+		,in USERID varchar(36)
+		,in PROJECTID varchar(36)
+	    ,in APPID varchar(36)
+)
+BEGIN	
+	declare MAXM int; 
+
+	call MZ_spYERASl ( YEAR );
+	-- SELECT * FROM tmpYEARS;
+	
+	DROP TEMPORARY TABLE IF EXISTS tmpMONEY;	
+	CREATE TEMPORARY TABLE tmpMONEY(
+			YEAR int
+			,M int
+			,MONTH varchar ( 10 )
+			,USERID varchar(36)
+			,USER varchar(45)			
+			,AVATAR varchar(50)	
+			,MONEYIN decimal(16,2)
+			,MONEYOUT decimal(16,2)
+	);	
+
+	 IF PROJECTID is not null THEN 
+		CASE PROJECTID  
+		  WHEN 'all' THEN 
+			call MZ_spMONEYl_all (YEAR, APPID);
+			
+			SELECT MAX(M) - 1 INTO MAXM FROM tmpMONEY;			
+
+			select YEAR, M, MONTH, USERID, USER, AVATAR, sum( MONEYIN ) MONEYIN, sum( MONEYOUT ) MONEYOUT, sum( MONEYIN ) - sum( MONEYOUT ) DELTA
+			,CASE
+				WHEN sum( MONEYIN ) <= 0 THEN 0
+				WHEN (sum( MONEYOUT ) <= 0 OR sum( MONEYIN ) > sum( MONEYOUT )) THEN 100
+				ELSE ROUND( (sum( MONEYOUT ) * 100) / sum( MONEYIN ), 0 )
+			 END PMONEYIN
+			,CASE 
+				WHEN sum( MONEYOUT ) <= 0 THEN 0				
+				WHEN (sum( MONEYIN ) <= 0 OR sum( MONEYOUT ) > sum( MONEYIN )) THEN 100
+				ELSE ROUND( (sum( MONEYIN ) * 100) / sum( MONEYOUT ), 0 )
+			 END PMONEYOUT
+			,CASE M				
+				WHEN MAXM THEN 1
+				ELSE 0
+			 END COLLAPSED
+			from tmpMONEY
+			GROUP BY YEAR, M, MONTH, USERID, USER, AVATAR
+			order by M desc, USER;
+
+		WHEN 'private' THEN 
+			call MZ_spMONEYl_private (YEAR, APPID);
+			
+			select YEAR, M, MONTH, USERID, USER, sum( MONEYOUT ) MONEYOUT
+			from tmpMONEY
+			GROUP BY YEAR, M, MONTH, USERID, USER
+			order by M desc, USER;			
+	    ELSE  
+			call MZ_spMONEYl_project (YEAR, PROJECTID, APPID);	    
+
+			select YEAR, M, MONTH, USERID, USER, sum( MONEYOUT ) MONEYOUT
+			from tmpMONEY
+			GROUP BY YEAR, M, MONTH, USERID, USER
+			order by M desc, USER;
+		END CASE;
+	END IF;
+
+	
+END; //
+DELIMITER //
---
+++ storedProcedure/MZ_spYERASl.sql
@@ -0,0 +1,61 @@
+DROP PROCEDURE IF EXISTS MZ_spYERASl;
+
+DELIMITER //
+CREATE PROCEDURE MZ_spYERASl (		
+		 in YEAR int	    
+)
+BEGIN
+	
+	DROP TEMPORARY TABLE IF EXISTS tmpYEARS;	
+	CREATE TEMPORARY TABLE tmpYEARS(
+			YEAR int
+			,M int
+			,MONTH varchar ( 10 )
+			,START date
+			,END date
+	);
+	
+	SET @startMonth = "";
+	select `VALUE` into @startMonth from MZ_CONFIG where USERID is null and `KEY` = 'startmonth';
+
+	SET @endMonth = "";
+	select `VALUE` into @endMonth from MZ_CONFIG where USERID is null and `KEY` = 'endmonth';
+
+	insert into tmpYEARS value (YEAR, 1, 'GENNAIO', CONCAT(CAST(YEAR AS CHAR(4)) , '-01-', CAST(@startMonth AS CHAR(2)))
+										 	      , CONCAT(CAST(YEAR AS CHAR(10)) , '-02-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 2, 'FEBBRAIO', CONCAT(CAST(YEAR AS CHAR(4)) , '-02-', CAST(@startMonth AS CHAR(2)))
+ 											    , CONCAT(CAST(YEAR AS CHAR(10)) , '-03-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 3, 'MARZO', CONCAT(CAST(YEAR AS CHAR(4)) , '-03-', CAST(@startMonth AS CHAR(2)))
+										 	    , CONCAT(CAST(YEAR AS CHAR(10)) , '-04-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 4, 'APRILE', CONCAT(CAST(YEAR AS CHAR(4)) , '-04-', CAST(@startMonth AS CHAR(2)))
+  										         , CONCAT(CAST(YEAR AS CHAR(10)) , '-05-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 5, 'MAGGIO', CONCAT(CAST(YEAR AS CHAR(4)) , '-05-', CAST(@startMonth AS CHAR(2)))
+											     , CONCAT(CAST(YEAR AS CHAR(10)) , '-06-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 6, 'GIUGNO', CONCAT(CAST(YEAR AS CHAR(4)) , '-06-', CAST(@startMonth AS CHAR(2)))
+											     , CONCAT(CAST(YEAR AS CHAR(10)) , '-07-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 7,' LUGLIO', CONCAT(CAST(YEAR AS CHAR(4)) , '-07-', CAST(@startMonth AS CHAR(2)))
+											     , CONCAT(CAST(YEAR AS CHAR(10)) , '-08-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 8, 'AGOSTO', CONCAT(CAST(YEAR AS CHAR(4)) , '-08-', CAST(@startMonth AS CHAR(2)))
+											     , CONCAT(CAST(YEAR AS CHAR(10)) , '-09-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 9, 'SETTEMBRE', CONCAT(CAST(YEAR AS CHAR(4)) , '-09-', CAST(@startMonth AS CHAR(2)))
+											        , CONCAT(CAST(YEAR AS CHAR(10)) , '-10-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 10, 'OTTOBRE', CONCAT(CAST(YEAR AS CHAR(4)) , '-10-', CAST(@startMonth AS CHAR(2)))
+											       , CONCAT(CAST(YEAR AS CHAR(10)) , '-11-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 11, 'NOVEMBRE', CONCAT(CAST(YEAR AS CHAR(4)) , '-11-', CAST(@startMonth AS CHAR(2)))
+											        , CONCAT(CAST(YEAR AS CHAR(10)) , '-12-', CAST(@endMonth AS CHAR(2))));
+
+	insert into tmpYEARS value (YEAR, 12, 'DICEMBRE', CONCAT(CAST(YEAR AS CHAR(4)) , '-12-', CAST(@startMonth AS CHAR(2)))
+											        , CONCAT(CAST(YEAR + 1 AS CHAR(10)) , '-01-', CAST(@endMonth AS CHAR(2))));
+	-- select * from tmpYEARS;
+END; //
+DELIMITER //
